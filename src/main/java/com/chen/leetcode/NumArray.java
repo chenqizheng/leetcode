9ac3bd588a44f806748ff6edcad342b86319bebb
Chenqizheng <qizhengchenbeijing@gmail.com>
2017-03-29 16:44:52 +0800

    3.29

---
+++ src/main/java/com/chen/leetcode/NumArray.java
@@ -0,0 +1,23 @@
+package com.chen.leetcode;
+
+/**
+ * herf:https://leetcode.com/problems/range-sum-query-immutable/#/description
+ */
+public class NumArray {
+
+    int[] nums;
+
+    public NumArray(int[] nums) {
+        for (int i = 1; i < nums.length; i++)
+            nums[i] += nums[i - 1];
+
+        this.nums = nums;
+    }
+
+    public int sumRange(int i, int j) {
+        if (i == 0)
+            return nums[j];
+
+        return nums[j] - nums[i - 1];
+    }
+}
