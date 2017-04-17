dd974d1995d82374c3953c66844cd4fc52f64b16
Chenqizheng <qizhengchenbeijing@gmail.com>
2017-04-17 16:38:20 +0800

    4.17  通过前序遍历 和中序遍历重建二叉树 todo

--- src/main/java/com/chen/leetcode/ReBuildBinaryTree.java
+++ src/main/java/com/chen/leetcode/ReBuildBinaryTree.java
@@ -4,44 +4,22 @@ package com.chen.leetcode;
  * 根据前序遍历、中序遍历重构二叉树
  * Created by Chen on 2017/4/17.
  */
-@Deprecated
 public class ReBuildBinaryTree {
 
     public TreeNode solution(int[] preOrder, int[] inOrder) {
-        TreeNode root = new TreeNode(preOrder[0]);
-        rebuildBinaryTree(root, preOrder, 0, preOrder.length - 1, inOrder, 0, inOrder.length - 1);
-        return root;
+        return rebuildBinaryTree(preOrder, 0, preOrder.length - 1, inOrder, 0, inOrder.length - 1);
     }
 
     //TODO 未完成
-    public void rebuildBinaryTree(TreeNode treeNode, int[] preOrder, int preOrderStart, int preOrderEnd, int[] inOrder, int inOrderStart, int inOrderEnd) {
-        if (inOrderStart == inOrderEnd || preOrderStart == preOrderEnd) {
-            if (findIndex(inOrder, 0, inOrder.length - 1, treeNode.val) > inOrderEnd) {
-                treeNode.left = new TreeNode(inOrder[inOrderEnd]);
-            } else {
-                treeNode.right = new TreeNode(inOrder[inOrderEnd]);
-            }
-            return;
-        }
-        int root = preOrder[preOrderStart];
-        int leftLenght = findIndex(inOrder, inOrderStart, inOrderEnd, root) - inOrderStart;
-        if (preOrderStart + 1 < preOrder.length) {
-            TreeNode left = new TreeNode(preOrder[preOrderStart + 1]);
-            treeNode.left = left;
-            if (preOrderStart <= leftLenght - 1) {
-                rebuildBinaryTree(left, preOrder, preOrderStart + 1, leftLenght + preOrderStart, inOrder, inOrderStart, inOrderStart + leftLenght - 1);
-            }
-        }
-
-        if (preOrderStart + leftLenght + 1 < preOrder.length) {
-            TreeNode right = new TreeNode(preOrder[preOrderStart + leftLenght + 1]);
-            treeNode.right = right;
-            if (preOrderStart + leftLenght + 1 <= preOrderEnd) {
-                rebuildBinaryTree(right, preOrder, preOrderStart + leftLenght + 1, preOrderEnd, inOrder, leftLenght + 1, inOrderEnd);
-            }
-        }
-
-
+    public TreeNode rebuildBinaryTree(int[] preOrder, int preOrderStart, int preOrderEnd, int[] inOrder, int inOrderStart, int inOrderEnd) {
+        TreeNode treeNode = new TreeNode(preOrder[preOrderStart]);
+        int index = findIndex(inOrder, inOrderStart, inOrderEnd, preOrder[preOrderStart]);
+        int leftlength = index - inOrderStart;
+        //构建 左子树
+        treeNode.left = rebuildBinaryTree(preOrder, preOrderStart + 1, preOrderStart + leftlength, inOrder, inOrderStart, inOrderStart + leftlength);
+        treeNode.right = rebuildBinaryTree(preOrder, preOrderStart + leftlength + 1, preOrderEnd, inOrder, index + 1, inOrderEnd);
+        //构建 右子树
+        return treeNode;
     }
 
     public int findIndex(int[] array, int start, int end, int target) {
