54b9e6ff27ed32bf8481bae96ff73611eac617b8
Chenqizheng <qizhengchenbeijing@gmail.com>
2017-04-19 23:38:17 +0800

    树

---
+++ src/main/java/com/chen/leetcode/SubTree.java
@@ -0,0 +1,16 @@
+package com.chen.leetcode;
+
+/**
+ * 判断是否是子树
+ * Created by Chen on 2017/4/19.
+ */
+public class SubTree {
+    public boolean isSubTree(TreeNode root, TreeNode subRoot) {
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
