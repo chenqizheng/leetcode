47a3dd9147d29bae51df2c84cc64ce504bbf7b5f
chenqizheng <qizhengchenbeijing@gmail.com>
2018-11-27 16:32:00 +0800

    链表是否有环

---
+++ src/main/java/com/chen/leetcode/LinkedListCycle.java
@@ -0,0 +1,17 @@
+package com.chen.leetcode;
+
+public class LinkedListCycle {
+
+    public boolean hasCycle(ListNode head) {
+        if(head==null) return false;
+        ListNode walker = head;
+        ListNode runner = head;
+        while(runner.next!=null && runner.next.next!=null) {
+            walker = walker.next;
+            runner = runner.next.next;
+            if(walker==runner) return true;
+        }
+        return false;
+    }
+}
+
