dbe1d61d5477b097b19b280603ede32983c3bef4
chenqizheng <qizhengchenbeijing@gmail.com>
2019-03-22 14:51:33 +0800

    自定义string比较器

---
+++ src/main/java/com/chen/leetcode/LargestNumber.java
@@ -0,0 +1,34 @@
+package com.chen.leetcode;
+
+import java.util.*;
+
+public class LargestNumber {
+
+    public static void main(String[] args) {
+        new LargestNumber().largestNumber(new int[]{2, 10});
+    }
+
+    public String largestNumber(int[] nums) {
+        List<String> list = new ArrayList<>();
+        for (int num : nums) {
+            list.add(String.valueOf(num));
+        }
+        Collections.sort(list, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                String s1 = o1 + o2;
+                String s2 = o2 + o1;
+                return s2.compareTo(s1);
+            }
+        });
+        StringBuilder sb = new StringBuilder();
+
+        if (list.get(0).equals("0")) {
+            return "0";
+        }
+        for (String str : list) {
+            sb.append(str);
+        }
+        return sb.toString();
+    }
+}
