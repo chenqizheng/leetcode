17ef5b145b42625ccaf7f574087c3cdcfc935db6
chenqizheng <qizhengchenbeijing@gmail.com>
2019-03-28 12:08:02 +0800

    对角线打印

--- src/main/java/com/chen/leetcode/DiagonalTraverse.java
+++ src/main/java/com/chen/leetcode/DiagonalTraverse.java
@@ -1,5 +1,8 @@
 package com.chen.leetcode;
 
+/***
+ * leetcode :https://leetcode.com/problems/diagonal-traverse/
+ */
 public class DiagonalTraverse {
 
 
@@ -11,18 +14,15 @@ public class DiagonalTraverse {
     }
 
     public int[] findDiagonalOrder(int[][] matrix) {
-        if (matrix == null) {
-            return null;
-        }
-        int maxRow = matrix.length;
-        int maxCol = matrix[0].length;
-        int[] result = new int[maxCol * maxRow];
-        int r = 0;
-        int c = 0;
-        boolean isUp = true;
-        for (int i = 0; i < result.length; i++) {
+        if (matrix == null || matrix.length == 0) return new int[0];
+        int m = matrix.length, n = matrix[0].length;
+
+        int[] result = new int[m * n];
+        int r = 0, c = 0;
+        for (int i = 0; i < m * n; i++) {
+            result[i] = matrix[r][c];
             if ((r + c) % 2 == 0) {
-                if (c == maxCol - 1) {
+                if (c == n - 1) {
                     ++r;
                 } else if (r == 0) {
                     ++c;
@@ -31,7 +31,7 @@ public class DiagonalTraverse {
                     ++c;
                 }
             } else {
-                if (r == maxRow - 1) {
+                if (r == m - 1) {
                     ++c;
                 } else if (c == 0) {
                     ++r;
@@ -40,7 +40,6 @@ public class DiagonalTraverse {
                     --c;
                 }
             }
-
         }
         return result;
     }
