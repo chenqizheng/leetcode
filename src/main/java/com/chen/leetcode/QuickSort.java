53bf548d5d22aaf389d7e252e26214dbafb528e9
chenqizheng <qizhengchenbeijing@gmail.com>
2019-04-02 20:39:43 +0800

    快排

---
+++ src/main/java/com/chen/leetcode/QuickSort.java
@@ -0,0 +1,38 @@
+
+package com.chen.leetcode;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] array = new int[]{7, 5, 2, 4, 1, 8, 9, 6};
+
+        new QuickSort().solution(array, 0, array.length - 1);
+        for (int i : array) {
+            System.out.println(i);
+        }
+    }
+
+    public void solution(int[] array, int low, int high) {
+        if (low > high) {
+            return;
+        }
+        int i = low;
+        int j = high;
+        int temp = array[low];
+        while (i < j) {
+            while (temp < array[j] && i < j) {
+                j--;
+            }
+            while (temp > array[i] && i < j) {
+                i++;
+            }
+
+            if (i < j) {
+                int t = array[i];
+                array[i] = array[j];
+                array[j] = t;
+            }
+        }
+        solution(array, low, j - 1);
+        solution(array, j + 1, high);
+    }
+}
