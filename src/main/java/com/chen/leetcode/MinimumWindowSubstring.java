5a059daac58953f16166480ae987a0250b3271be
chenqizheng <chenqizheng@jd.com>
2020-05-06 15:00:50 +0800

    最短子序列

---
+++ src/main/java/com/chen/leetcode/MinimumWindowSubstring.java
@@ -0,0 +1,63 @@
+package com.chen.leetcode;
+
+import java.util.HashMap;
+
+public class MinimumWindowSubstring {
+
+    private static String minWindow(String s1, String s2) {
+        int left = 0;
+        int right = 0;
+        int vaild = 0;
+        HashMap<Character, Integer> needs = new HashMap<>();
+        HashMap<Character, Integer> windows = new HashMap<>();
+        for (char c : s2.toCharArray()) {
+            if (needs.containsKey(c)) {
+                needs.put(c, needs.get(c) + 1);
+            } else {
+                needs.put(c, 1);
+            }
+        }
+        int start = 0;
+        int length = Integer.MAX_VALUE;
+        while (right < s1.length()) {
+            char c = s1.charAt(right);
+            right++;
+            if (needs.containsKey(c)) {
+                if (windows.containsKey(c)) {
+                    windows.put(c, windows.get(c) + 1);
+                } else {
+                    windows.put(c, 1);
+                }
+                if (windows.get(c) == needs.get(c)) {
+                    vaild++;
+                }
+            }
+            while (vaild == needs.size()) {
+                if (right - left < length) {
+                    start = left;
+                    length = right - left;
+                }
+                char d = s1.charAt(left);
+                left++;
+                if (needs.containsKey(d)) {
+                    if (needs.get(d) == windows.get(d)) {
+                        vaild--;
+                    }
+                    if (windows.get(d) > 0) {
+                        windows.put(d, windows.get(d) - 1);
+                    }
+                }
+            }
+        }
+        if (length == Integer.MAX_VALUE) {
+            return "";
+        } else {
+            return s1.substring(start,start + length);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(minWindow("EBBANCF", "ABC"));
+    }
+}
